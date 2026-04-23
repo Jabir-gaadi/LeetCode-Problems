f2287246db1c5d186521a989603f61ded4ba6a8e
Jabir-gaadi <pp8nt5zb4q@privaterelay.appleid.com>
2026-04-23 15:12:24 +0100

    Time: N/A (0%) | Memory N/A (0%) .py

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -2,4 +2,4 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
-        return list(map(int, str(x))) == list(map(int, str(x)))[::-1]
+        return str(x) == str(x)[::-1]
